93a697c88f283640d5b655b6af6d46fc8b75924a
NBarnes <nbarnes@gmail.com>
2013-10-27 12:34:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Overview
+========
+
+A basic Q-and-A web app.  Created by Nathaniel Barnes as part of CodeFellow's
+Ruby on Rails bootcamp, October 2013.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QAndA::Application.config.secret_token = '53168502f2427b3537ae02d23ea3dac6040615cedaddc030a0729cf26eb55bc3fc7ec150d3c6dfbf9d33fe52053c56be1a2bccd501657bc931c59fc15e5fda31'
